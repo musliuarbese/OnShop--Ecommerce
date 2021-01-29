0034ca00fcee4ebdae6c518a75bbff29b2f00383
musliuarbese <63508538+musliuarbese@users.noreply.github.com>
2021-01-30 00:17:29 +0100

    Fetching products and users to database

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,14 +1,15 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+import { createLogger } from 'redux-logger'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 
 const reducer = combineReducers({})
-
+const logger = createLogger();
 const initialState = {}
 
 const store = createStore(reducer, 
     initialState, 
-    composeWithDevTools(applyMiddleware([thunk])))
+    composeWithDevTools(applyMiddleware(logger, thunk)))
 
 export default store
